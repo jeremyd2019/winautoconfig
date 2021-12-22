d8fe839bdcc43706e0ded3aa9d5c13cc65e658c2
Jeremy Drake <github@jdrake.com>
2021-12-22 11:34:35 -0800

    Use wimlib's wimcapture instead of DISM
    There was an issue with the ACLs that caused UWP apps to fail to launch
    if the image was both created and deployed by DISM.  wimcapture has an
    option to omit ACLs, so use that, and re-enable deploying image with
    DISM in deploy.cmd.

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 set DRIVE=%1
 
-REM DISM /Apply-Image /ImageFile:dataimage_arm64.wim /Index:1 /ApplyDir:%DRIVE%
+DISM /Apply-Image /ImageFile:dataimage_arm64.wim /Index:1 /ApplyDir:%DRIVE%
 DISM /Image:%DRIVE% /Add-Driver /Driver:"%DRIVE%Windows\ConfigSetRoot\AutoUnattend_Files\winautoconfig\dist\Out-of-Box Drivers\arm64" /Recurse /ForceUnsigned
 REM DISM /Image:%DRIVE% /Add-Capability /CapabilityName:OpenSSH.Server~~~~0.0.1.0 /Source:"%DRIVE%Windows\ConfigSetRoot\AutoUnattend_Files\dist\Packages\OnDemandPack\arm64_OpenSSH-Server-Package_10.0.22000.1_neutral_31bf3856ad364e35_"
--- mkdataimage.cmd
+++ mkdataimage.cmd
@@ -13,14 +13,4 @@ xcopy /h/e %_confset%\AutoUnattend_Files dataimage\Windows\ConfigSetRoot\AutoUna
 copy /y "%_confset%\$OEM$\$$\OEM\CustomizationFiles\*.*" dataimage\Windows\OEM\CustomizationFiles
 copy /y "%_confset%\$OEM$\$$\OEM\CustomizationFiles\*.*" dataimage\Recovery\AutoApply\CustomizationFiles
 
-icacls dataimage\Recovery\Customizations /grant:r *S-1-5-32-544:(F) /T
-icacls dataimage\Recovery\Customizations /inheritance:r /T
-icacls dataimage\Recovery\Customizations /grant:r SYSTEM:(F) /T
-icacls dataimage\Recovery\OEM /grant:r *S-1-5-32-544:(F) /T
-icacls dataimage\Recovery\OEM /inheritance:r /T
-icacls dataimage\Recovery\OEM /grant:r SYSTEM:(F) /T
-icacls dataimage\Recovery\AutoApply /grant:r *S-1-5-32-544:(F) /T
-icacls dataimage\Recovery\AutoApply /inheritance:r /T
-icacls dataimage\Recovery\AutoApply /grant:r SYSTEM:(F) /T
-
-dism /Capture-Image /ImageFile:dataimage.wim /CaptureDir:dataimage /Name:DataImage
+CALL wimcapture.cmd dataimage dataimage.wim DataImage --no-acls
