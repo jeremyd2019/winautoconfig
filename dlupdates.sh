93bdbe47a461bbd5431375bf190fa78f3a7fe7f2
Jeremy Drake <github@jdrake.com>
2022-01-31 16:06:54 -0800

    Add script to download Windows updates
    list manually maintained on a wiki page for now.  Maybe one of the UUP
    sites has a mechanism to get an automatic list.

---
+++ dlupdates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p updates && cd updates
+curl https://github.com/jeremyd2019/winautoconfig/wiki/Windows-11-ARM64-updates | grep -Eo 'http://download\.windowsupdate\.com/.*\.msu' | wget -i -
+cd ..
+genisoimage -iso-level 4 -J -o updates.iso updates/
+
