28a3540b9984ebeb43225f251824039e3ca69848
Jeremy Drake <github@jdrake.com>
2023-06-20 16:18:27 -0700

    update dlupdates.sh
    Adjust pattern for new url
    https://catalog.sf.dl.delivery.mp.microsoft.com/... used for cumulative
    updates for Windows 11 22H2.
    
    Update wiki url to Windows 11 22H2 updates.

--- dlupdates.sh
+++ dlupdates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p updates && cd updates
-curl https://github.com/jeremyd2019/winautoconfig/wiki/Windows-11-ARM64-updates | grep -Eo 'https://catalog\.s\.download\.windowsupdate\.com/.*\.msu' | wget -i -
+curl https://github.com/jeremyd2019/winautoconfig/wiki/Windows-11-22H2-ARM64-updates | grep -Eo 'https://catalog\.(s\.download\.windowsupdate|sf\.dl\.delivery\.mp\.microsoft)\.com/.*\.msu' | wget -i -
 cd ..
 genisoimage -iso-level 4 -J -o updates.iso updates/
 
