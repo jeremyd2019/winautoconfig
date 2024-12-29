d4a11a54efb8725c7bd9b6e21b893197bbdb7add
Jeremy Drake <github@jdrake.com>
2024-12-28 19:53:54 -0800

    clean up some additional locations under USERPROFILE

--- msys2-runner-setup/setupscripts/runnerpost.ps1
+++ msys2-runner-setup/setupscripts/runnerpost.ps1
@@ -5,3 +5,7 @@ if (test-path c:\msys64\var\lib\pacman\db.lck) {
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'pacman --noconfirm --noprogressbar -Syuu'
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'pacman --noconfirm --noprogressbar -Suu'
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'set -x; pacman --noconfirm --noprogressbar -Rnu $(pacman -Qq | grep -xvFf "$RUNNER_TEMP/_original_pkglist")'
+# clean up some trash under USERPROFILE
+if (test-path "$env:USERPROFILE\.cache") { rm -r -fo "$env:USERPROFILE\.cache" }
+if (test-path "$env:USERPROFILE\.cargo") { rm -r -fo "$env:USERPROFILE\.cargo" }
+if (test-path "$env:USERPROFILE\go") { rm -r -fo "$env:USERPROFILE\go" }
