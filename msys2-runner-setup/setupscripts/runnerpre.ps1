3419a788ac86dc73ed55776f13916cde148d3be7
Jeremy Drake <github@jdrake.com>
2025-01-06 22:03:10 -0800

    suppress output from mkdir in powershell

--- msys2-runner-setup/setupscripts/runnerpre.ps1
+++ msys2-runner-setup/setupscripts/runnerpre.ps1
@@ -1,4 +1,4 @@
-mkdir -force "$env:RUNNER_TEMP\_runner_hook_state"
+mkdir -force "$env:RUNNER_TEMP\_runner_hook_state" | Out-Null
 copy "c:\msys64\etc\pacman.conf" "$env:RUNNER_TEMP\_runner_hook_state\"
 copy "c:\msys64\etc\pacman.d\mirrorlist.*" "$env:RUNNER_TEMP\_runner_hook_state\"
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'pacman -Qq > "$RUNNER_TEMP/_runner_hook_state/original_pkglist"'
