2b5e6a0931f0cecabd242132e70428995ccc84b3
Jeremy Drake <github@jdrake.com>
2025-01-14 13:03:44 -0800

    Remove packages before potentially downgrading.
    Generally, the procedure is: update from staging, then build pacakge
    (which installs dependencies).  So the reverse should make the most
    sense to undo it.  Besides, it avoids downloading/downgrading a package
    we're just going to uninstall.

--- msys2-runner-setup/setupscripts/runnerpost.ps1
+++ msys2-runner-setup/setupscripts/runnerpost.ps1
@@ -4,9 +4,9 @@ if (test-path c:\msys64\var\lib\pacman\db.lck) {
 }
 copy -force "$env:RUNNER_TEMP\_runner_hook_state\pacman.conf" "c:\msys64\etc\pacman.conf"
 copy -force "$env:RUNNER_TEMP\_runner_hook_state\mirrorlist.*" "c:\msys64\etc\pacman.d\"
+c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'set -x; pacman --noconfirm --noprogressbar -Rnu $(pacman -Qq | grep -xvFf "$RUNNER_TEMP/_runner_hook_state/original_pkglist")'
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'pacman --noconfirm --noprogressbar -Syuu'
 c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'pacman --noconfirm --noprogressbar -Suu'
-c:\msys64\msys2_shell.cmd -defterm -no-start -msys2 -c 'set -x; pacman --noconfirm --noprogressbar -Rnu $(pacman -Qq | grep -xvFf "$RUNNER_TEMP/_runner_hook_state/original_pkglist")'
 # clean up some trash under USERPROFILE
 if (test-path "$env:USERPROFILE\.cache") { cmd /c "rmdir /s/q `"$env:USERPROFILE\.cache`"" }
 if (test-path "$env:USERPROFILE\.cargo") { cmd /c "rmdir /s/q `"$env:USERPROFILE\.cargo`"" }
